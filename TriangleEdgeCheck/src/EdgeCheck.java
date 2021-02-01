462dcf2142e59eba5aa5a28434f9c1a7be93ef0e
ghimani614 <ghimani614@gmail.com>
2021-02-01 16:10:03 -0500

    Checking sides of Triangle

---
+++ TriangleEdgeCheck/src/EdgeCheck.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class EdgeCheck {
+
+	public static void main(String[] args) {
+		int sideA, sideB, sideC;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first side of Triangle");
+		sideA = sc.nextInt();
+		System.out.println("Enter second side of Triangle");
+		sideB = sc.nextInt();		
+		System.out.println("Enter third side of Triangle");
+		sideC = sc.nextInt();		
+		checkIfTriangleCanBeFormed(sideA, sideB, sideC);
+
+	}
+
+	private static void checkIfTriangleCanBeFormed(int sideA, int sideB, int sideC) {
+		if((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))
+		{
+			System.out.println("Triangle can be formed with given lengths");
+		}
+		else
+			System.out.println("Triangle creation is not possible with given lengths");
+	}
+
+}
